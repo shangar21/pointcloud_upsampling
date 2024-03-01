6b40260b25702b6fda7255963e4caaeb639cedf7
shangar <sharanshangar@gmail.com>
2024-03-01 13:01:47 -0500

    progress

--- sample_pc.py
+++ sample_pc.py
@@ -27,14 +27,16 @@ print("Point cloud sample shape: ", point_cloud_sample.shape)
 
 oct = octree.Octree()
 oct.create(point_cloud_sample)
-N = 5
+N = 3
 for i in range(N):
     for point in point_cloud_sample:
         parent = oct.deepest_parent(point)
         parent_dims = np.array([parent.width, parent.height, parent.depth])
         direction = np.random.randint(0, 8)
         direction = octree.DIRECTION_TO_COORDINATES[direction]
-        oct.insert(oct.tree, parent.center + np.multiply(parent_dims / 2, direction))
+        magnitude = np.array([np.random.uniform(low=0, high=i) for i in parent_dims])
+        oct.insert(oct.tree, point + np.multiply(magnitude, direction))
+#        oct.insert(oct.tree, parent.center + np.multiply(parent_dims / 2, direction))
 
 new_points = np.array(oct.get_points())
 print("New points shape: ", new_points.shape)
