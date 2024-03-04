b7107347eb999a291c472079e2b629e42ff8c482
shangar21 <sharanshangar@gmail.com>
2024-03-03 21:23:55 -0500

    bilateral filtering, performs quite well

--- sample_pc.py
+++ sample_pc.py
@@ -4,6 +4,7 @@ import sys
 import octree
 import open3d as o3d
 import smoothing
+import time
 
 def visualize_point_cloud(point_cloud):
     fig = plt.figure()
@@ -22,15 +23,19 @@ def estimate_normals(point_cloud):
     normals = np.asarray(pcd.estimate_normals())
     return normals 
 
+def estimate_normals(point_cloud):
+    pcd = o3d.geometry.PointCloud()
+    pcd.points = o3d.utility.Vector3dVector(point_cloud)
+    pcd.estimate_normals()
+    normals = np.asarray(pcd.normals)
+    return normals
+
 point_cloud_path = sys.argv[1]
 point_cloud = np.load(point_cloud_path)
-
-print("Point cloud shape: ", point_cloud.shape)
 
 # sample 1024 random points
 point_cloud_sample = point_cloud[np.random.choice(point_cloud.shape[0], 1024, replace=False), :]
 #visualize_numpy_pointcloud_o3d(point_cloud_sample)
-print("Point cloud sample shape: ", point_cloud_sample.shape)
 
 oct = octree.Octree()
 oct.create(point_cloud_sample)
@@ -44,12 +49,20 @@ for i in range(N):
         magnitude = np.array([np.random.uniform(low=0, high=i) for i in parent_dims])
         oct.insert(oct.tree, point + np.multiply(magnitude, direction))
 
-print("New points shape: ", point_cloud_sample.shape)
 
 visualize_numpy_pointcloud_o3d(oct.get_points())
 
+start = time.time()
 point_cloud_sample = smoothing.nearest_neighbours_smoothing(np.array(oct.get_points()), k=30)
+end = time.time()
+print("KNN smoothing Time: ", end - start)
+visualize_numpy_pointcloud_o3d(point_cloud_sample)
+
+#point_cloud_sample = smoothing.moving_least_squares_smoothing(np.array(oct.get_points()), k=30, degree=2)
 #visualize_numpy_pointcloud_o3d(point_cloud_sample)
 
-point_cloud_sample = smoothing.moving_least_squares_smoothing(np.array(oct.get_points()), k=30, degree=2)
+start = time.time()
+point_cloud_sample = smoothing.bilateral_smoothing(np.array(oct.get_points()), k=30, sigma_d=0.1, sigma_n=0.1)
+end = time.time()
+print("Bilateral smoothing Time: ", end - start)
 visualize_numpy_pointcloud_o3d(point_cloud_sample)
--- smoothing.py
+++ smoothing.py
@@ -48,13 +48,76 @@ def fit_plane(neighbours, point):
     # return the value of the plane at the point
     return x[0]*point[0] + x[1]*point[1] + x[2]
 
+#
+#def get_polynomial_surface(points, coefs, degree=2):
+#    """
+#    This function returns the value of the polynomial surface at the points.
+#    """
+#    poly = PolynomialFeatures(degree=degree)
+#    X = points[:, [0, 1]]
+#    X_ = poly.fit_transform(X)
+#    poly_vals = np.dot(X_, coefs).reshape(-1, 1)
+#    return np.hstack((points[:, [0,1]], poly_vals))
+#
+#
+#def fit_point(points, point, degree=2, target_dim=3):
+#    """
+#    This function fits a polynomial to the points and returns the value of the polynomial at the point.
+#    """
+#    # create polynomial features
+#    target_dim -= 1
+#    poly = PolynomialFeatures(degree=degree)
+#    X = points[:, [i for i in range(3) if i != target_dim]]
+#    y = points[:, target_dim]
+#    X = poly.fit_transform(X)
+#    # fit the polynomial
+#    model = LinearRegression().fit(X, y)
+#    # find closest point to polynomial
+#    coefs = model.coef_
+#    surface = get_polynomial_surface(points, coefs, degree)
+#    idx = np.argmin(np.linalg.norm(point - surface, axis=1))
+#    return surface[idx]
+
 def moving_least_squares_smoothing(point_cloud, k=5, degree=2):
     tree = KDTree(point_cloud)
     for i in tqdm(range(point_cloud.shape[0])):
         indices = tree.query(point_cloud[i].reshape(1, -1), k=k+1)[1][0]
         indices = indices[1:]
         point = point_cloud[i].reshape(1, -1)
-        point_cloud[i, 2] = fit_plane(point_cloud[indices], point)
-        #point_cloud[i, 2] = fit_polynomial(point_cloud[indices], point, degree=degree)
+        #point_cloud[i, 2] = fit_plane(point_cloud[indices], point)
+        point_cloud[i, 2] = fit_polynomial(point_cloud[indices], point, degree=degree)
     return point_cloud
+
+def regression_plane(neighbours):
+    mean = np.mean(neighbours, axis=0)
+    centered_neighbours = neighbours - mean
+    covariance_matrix = np.cov(centered_neighbours, rowvar=False)
+    ev, evec = np.linalg.eig(covariance_matrix)
+    normal = evec[:, np.argmin(ev)]
+    return mean, normal
+
+def bilateral_smoothing(point_cloud, k=5, sigma_d=0.1, sigma_n=0.1):
+    """
+    This function performs bilaterial filtering on a point cloud.
+    """
+    # create KDTree from the point cloud
+    tree = KDTree(point_cloud)
+    # Iterate over each point in the point cloud
+    for i in tqdm(range(point_cloud.shape[0])):
+        # get the k nearest neighbours
+        indices = tree.query(point_cloud[i].reshape(1, -1), k=k+1)[1][0]
+        indices = indices[1:]
+        neighbours = point_cloud[indices]
+        mean, normal = regression_plane(neighbours)
+        sum_of_weights = 0
+        delta_p = 0
+        for q in neighbours:
+            d_d = np.linalg.norm(q - point_cloud[i])
+            d_n = np.dot(normal, q - point_cloud[i])
+            w = np.exp(-d_d**2/(2*sigma_d**2) - d_n**2/(2*sigma_n**2))
+            delta_p = delta_p + (w * d_n)
+            sum_of_weights = sum_of_weights + w
+        point_cloud[i] = point_cloud[i] + ((delta_p / sum_of_weights ) * normal)
+    return point_cloud
+
 
